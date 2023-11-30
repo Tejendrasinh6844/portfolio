6457fa8d7f90b1e28b7126ff8d026e760ee2d4ad
Tejendrasinh <tejendrasinh6844@gmail.com>
2023-11-30 10:59:16 +0530

    disable route

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Preloader from '../src/components/Pre';
-import Home from './pages/Home';
-import About from './pages/About';
-// import Project from './pages/Project';
-import Contact from './pages/Contact';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Cursor from '../src/components/Cursor';
+// import { useEffect, useState } from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Preloader from '../src/components/Pre';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// // import Project from './pages/Project';
+// import Contact from './pages/Contact';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+// import Cursor from '../src/components/Cursor';
 
 function App() {
-  const [load, updateLoad] = useState(true);
+  // const [load, updateLoad] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      updateLoad(false);
-    }, 1200);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     updateLoad(false);
+  //   }, 1200);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
-    <div className="App">
-      <Cursor />
+    <div className="App">hello
+      {/* <Cursor />
       <Router>
         <Preloader load={load} />
         <div className="App" id={load ? 'no-scroll' : 'scroll'}>
@@ -30,10 +30,10 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             {/* <Route path="/project" element={<Project />} /> */}
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-      </Router>
+            {/* <Route path="/contact" element={<Contact />} /> */}
+          {/* </Routes> */}
+        {/* </div> */}
+      {/* </Router> */} */}
     </div>
   );
 }
